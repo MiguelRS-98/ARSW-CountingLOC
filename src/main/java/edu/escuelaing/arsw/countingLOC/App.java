0fb6ac11d9dcae13a2d591f302f7736ee9c8758d
Miguel Rodríguez <miguel.rodriguez-s@mail.escuelaing.edu.co>
2021-06-01 17:26:23 -0500

    LOC Counting

---
+++ src/main/java/edu/escuelaing/arsw/countingLOC/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.countingLOC;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
